6c6bc3043bbf07b64cf06e457022fa6506145d9f
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-14 04:03:12 +0530

    Update cashier.php

--- cashier.php
+++ cashier.php
@@ -31,6 +31,12 @@
         <h1 class="display-4">Cashier Page</h1>
         <p class="lead">Please use the following tools provided to re adjust pharmacy's inventory after receiving the cash</p>
     </div>
+
+    <div class="container col-lg-7 col-xl-7 text-center text-justify my-4">
+        <h1 class="display-4">View medicine bill</h1>
+        <p class="lead">Please use the following link to access the tools to manage pharmacy's prescription bill</p>
+        <a href="vieworder.php" class="btn btn-outline-primary">View prescription bills</a>
+    </div>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
