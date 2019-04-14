7f3d3f8c3217afb7404f35e8e9905f531cb20ac9
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-14 19:06:40 +0530

    Update cashier.php

--- cashier.php
+++ cashier.php
@@ -18,7 +18,10 @@
                     <div class="navbar-menu ml-auto">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
+                                <a class="nav-link" href="vieworder.php">Order's</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="cashier.php">Home</a>
                             </li>
                         </ul>
                     </div>
