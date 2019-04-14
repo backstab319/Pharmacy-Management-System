cc3e2daa310a888611021f6f93b5824bbdc5346e
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-14 05:34:55 +0530

    Update vieworder.php

--- vieworder.php
+++ vieworder.php
@@ -66,6 +66,26 @@
             ?>
         </div>
     </div>
+
+    <div class="container text-center my-4 col-lg-7 col-xl-7">
+        <h1 class="display-4">Clear prescription order's</h1>
+        <div class="form-group">
+            <form action="vieworder.php" method="POST">
+                <input type="submit" value="Clear" name="clear" class="btn btn-outline-primary">
+            </form>
+        </div>
+        <?php
+            if(isset($_POST["clear"])){
+                clear();
+            }
+            function clear(){
+                global $conn;
+                $sql = "TRUNCATE TABLE order_med";
+                $conn->query($sql);
+                header("Location: vieworder.php");
+            }
+        ?>
+    </div>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
